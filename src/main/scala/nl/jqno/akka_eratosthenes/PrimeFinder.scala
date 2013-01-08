c0934b4a725451b076cb469da2c897ddf4e4474f
Jan Ouwens <jan.ouwens@gmail.com>
2013-01-08 23:13:11 +0100

    Restructured the actors

--- src/main/scala/nl/jqno/akka_eratosthenes/PrimeFinder.scala
+++ src/main/scala/nl/jqno/akka_eratosthenes/PrimeFinder.scala
@@ -10,26 +10,24 @@ import akka.pattern.ask
 import akka.routing.RoundRobinRouter
 import akka.util.Timeout
 import akka.util.duration.intToDurationInt
+import scala.collection.mutable.HashMap
 
-class PrimeFinder(upper: Int, nrOfWorkers: Int = 2) {
-  val system = ActorSystem("EratosthenesSystem")
-  val master = system.actorOf(Props(new Master), name = "master")
-  val queuer = system.actorOf(Props(new Queuer), name = "queuer")
-  val worker = system.actorOf(Props(new Worker) withRouter RoundRobinRouter(nrOfWorkers), name = "worker")
+object PrimeFinder {
+  type ActorRefs = collection.mutable.Map[String, ActorRef]
   
-  private class Master extends Actor {
+  class Master(upper: Int, actors: ActorRefs) extends Actor {
     var source: ActorRef = _
     
     def receive = {
       case Find =>
         source = sender
-        (2 to upper) foreach { n => worker ! Filter(n) }
+        (2 to upper) foreach { n => actors("worker") ! Filter(n) }
       case Result(list) =>
         source ! list
     }
   }
   
-  private class Queuer extends Actor {
+  class Queuer(upper: Int, actors: ActorRefs) extends Actor {
     private var other: Option[List[Int]] = None
     private var expectedMessages = ((upper - 1) * 2) - 1
     
@@ -37,7 +35,7 @@ class PrimeFinder(upper: Int, nrOfWorkers: Int = 2) {
       case Enqueue(list) =>
         expectedMessages -= 1
         if (expectedMessages == 0) {
-          master ! Result(list)
+          actors("master") ! Result(list)
         }
         else {
           process(list)
@@ -47,7 +45,7 @@ class PrimeFinder(upper: Int, nrOfWorkers: Int = 2) {
     private def process(list: List[Int]) {
       other match {
         case Some(o) =>
-          worker ! Merge(list, o)
+          actors("worker") ! Merge(list, o)
           other = None
         case None =>
           other = Some(list)
@@ -55,18 +53,31 @@ class PrimeFinder(upper: Int, nrOfWorkers: Int = 2) {
     }
   }
   
-  private class Worker extends Actor {
+  class Worker(upper: Int, actors: ActorRefs) extends Actor {
     def receive = {
       case Filter(n) =>
-        queuer ! Enqueue((2 to upper).toList filter (x => x == n || x % n != 0))
+        actors("queuer") ! Enqueue((2 to upper).toList filter (x => x == n || x % n != 0))
       case Merge(left, right) =>
-        queuer ! Enqueue(left intersect right)
+        actors("queuer") ! Enqueue(left intersect right)
     }
   }
   
-  def start: List[Int] = {
-    implicit val timeout = Timeout(20 seconds)
-    val future = master ? Find
-    Await.result(future, timeout.duration).asInstanceOf[List[Int]]
+  def apply(upper: Int, nrOfWorkers: Int = 2): List[Int] = {
+    if (upper < 2)
+      Nil
+    else {
+      val actors = new HashMap[String, ActorRef]
+      val system = ActorSystem("EratosthenesSystem")
+      
+      val master = system.actorOf(Props(new Master(upper, actors)), name = "master")
+      val queuer = system.actorOf(Props(new Queuer(upper, actors)), name = "queuer")
+      val worker = system.actorOf(Props(new Worker(upper, actors)) withRouter RoundRobinRouter(nrOfWorkers), name = "worker")
+      
+      actors += ("master" -> master, "queuer" -> queuer, "worker" -> worker)
+  
+      implicit val timeout = Timeout(20 seconds)
+      val future = master ? Find
+      Await.result(future, timeout.duration).asInstanceOf[List[Int]]
+    }
   }
 }
--- src/main/scala/nl/jqno/akka_eratosthenes/Runner.scala
+++ src/main/scala/nl/jqno/akka_eratosthenes/Runner.scala
@@ -2,7 +2,6 @@ package nl.jqno.akka_eratosthenes
 
 object Runner extends App {
   val upper = 30
-  val finder = new PrimeFinder(upper)
-  val primes = finder.start
+  val primes = PrimeFinder(upper)
   println(primes)
 }
